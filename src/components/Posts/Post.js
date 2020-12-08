873562ee4d9f3f8c63cbdfb3e7836de61b8191a8
RubesWorld <ra15@outlook.com>
2020-12-08 13:52:16 -0800

    Completed MVP

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,25 +5,25 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { postsData, likePostFunc } = props;
 
   return (
     <div className='post-border'>
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        username={postsData.username}
+        thumbnailUrl={postsData.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={postsData.imageUrl}
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePostFunc(postsData.id)} numberOfLikes={postsData.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={postsData.comments} />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,13 +4,20 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { postsData } = props
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
-    </div>
+    {postsData.map((postObj)=>{
+      return <Post
+        postsData={postObj}
+        />
+    })
+  }
+    
+      </div>
   );
 };
 
